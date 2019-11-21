3e078e294bca264849a08019744fa8f018ca40ba
MORE Robotics 1714 <robotics1714@gmail.com>
2019-11-20 19:53:55 -0600

    Set up lift states class

---
+++ 2019 Code Deep Space/src/main/java/frc/robot/commands/LiftStates.java
@@ -0,0 +1,33 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import frc.robot.Robot;
+
+/**
+ * Move the elevator to a given location. This command finishes when it is
+ * within the tolerance, but leaves the PID loop running to maintain the
+ * position. Other commands using the elevator should make sure they disable
+ * PID!
+ */
+public class LiftStates extends Command {
+  private final double m_setpoint;
+
+@Override
+protected boolean isFinished() {
+	return false;
+}
+
+  
+  }
+
+  
+  
+}
